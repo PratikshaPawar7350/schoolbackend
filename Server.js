aedbc5522bede973ad3d6aaaaeb6e17d9dc54748
PratikshaPawar7350 <107267479+PratikshaPawar7350@users.noreply.github.com>
2024-05-02 17:48:50 +0530

    add

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+require('dotenv').config();
 const app = express();
 const port = 4000;
 
