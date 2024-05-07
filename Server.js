2f5d63de2087983a6a5cc85908bc0b2ca199fac2
PratikshaPawar7350 <107267479+PratikshaPawar7350@users.noreply.github.com>
2024-05-07 10:45:36 +0530

    add

--- Server.js
+++ Server.js
@@ -59,7 +59,7 @@ app.get('/chapter', async (req, res) => {
 
 function bufferToBase64(buffer) {
   if (!buffer || !Buffer.isBuffer(buffer)) {
-    return null; // Handle case where buffer is null, undefined, or not a Buffer
+    return null;
   }
   return buffer.toString('base64');
 }
@@ -75,7 +75,7 @@ app.get('/syllabus', (req, res) => {
     }
 
     // Map results and convert image data to base64
-    const syllabusData = results.map(syllabus => ({
+    const syllabusData = results.rows.map(syllabus => ({
       id: syllabus.id,
       syllabusname: syllabus.syllabusname,
       // Convert image buffer to base64
@@ -85,7 +85,6 @@ app.get('/syllabus', (req, res) => {
     res.json(syllabusData);
   });
 });
-
 
 
 
